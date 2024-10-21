0013f41eda0e863319319ad06c097c3b77ed7565
DemmyJay <demmyjyd@gmail.com>
2024-10-21 12:07:52 +0100

    add product.css

--- admin_dashboard/products.php
+++ admin_dashboard/products.php
@@ -12,7 +12,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Products</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/product.css">
 </head>
 <body>
     <h2>All Products</h2>
